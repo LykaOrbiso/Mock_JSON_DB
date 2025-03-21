43660122356433d109a10ee6e46f51605e156af9
LykaOrbiso <lporbiso25@gmail.com>
2025-03-20 18:53:36 -0600

    UPDATE

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/users.js');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
@@ -8,7 +9,9 @@ const PORT = process.env.PORT;
 app.set('view engine','ejs');
 app.set('views', './views');
 
-
+//Middleware the view engine
+app.use(express.static('public'));
+app.use(userRoutes);
 app.listen(PORT, ()=>{
     console.log(`Connected to the port: ${PORT}`);
 });
