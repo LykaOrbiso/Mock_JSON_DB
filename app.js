ef9b9c187547d0f27fdb987b3646169a14a57077
LykaOrbiso <lporbiso25@gmail.com>
2025-04-01 18:36:56 -0600

    UPDATE

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express();
 const userRoutes = require('./routes/users.js');
 require('dotenv').config();
 const PORT = process.env.PORT;
-
+const rateLimit = require('express-rate-limit');
+const FixedWindowRateLimit = rateLimit({
+    windowsMs: 1 * 15 * 1000,
+    max: 10, 
+    message: 'Too many request. Please try again later.',
+});
 
 // set up the new engine
 app.set('view engine','ejs');
@@ -11,7 +16,9 @@ app.set('views','./views');
 
 //middleware to serve static files
 app.use(express.static('public'));
+app.use(FixedWindowRateLimit);
 app.use(userRoutes);
+
 
 app.listen(PORT,()=>{
     console.log(`connected on port: ${PORT}`);
